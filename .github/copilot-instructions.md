85096d57e248832e5345d6f183333613031d4abe
gaggarshreyash124 <62458178+gaggarshreyash124@users.noreply.github.com>
2025-05-14 09:29:24 +0530

    f

---
+++ .github/copilot-instructions.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: Endless-Climber
+- Unity version: Unity 2022.3.44f1
+- Active game object:
+  - Name: Frame
+  - Tag: Untagged
+  - Layer: UI
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,6 +13,9 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10f;
     public LayerMask groundLayer;
     public GameObject GroundCheck;
+    Animator anim;
+    public TextMeshProUGUI score;
+    public float highestPoint;
 
     public bool isGrounded()
     {
@@ -20,16 +26,48 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         Mybody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
+        float xvalue = Input.GetAxisRaw("Horizontal");
         Mybody.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Mybody.velocity.y);
 
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
             Mybody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
             Debug.Log("Jump");
+            
         }
+
+        if (isGrounded())
+        {
+            anim.SetBool("isjumping", false);
+        }
+        else if (!isGrounded())
+        {
+            anim.SetBool("isjumping",true);
+        }
+        if (xvalue > 0)
+        {
+            transform.localScale = new Vector3(5.5f, 5.5f, 5.5f);
+        }
+        else if (xvalue < 0)
+        {
+            transform.localScale = new Vector3(-5.5f, 5.5f, 5.5f);
+        }
+        anim.SetBool("Running",xvalue != 0);
+
+        if (transform.position.y >=0)
+        {
+            highestPoint = MathF.Round(transform.position.y/6.2f);
+            score.text = "Highest reached - " + highestPoint;
+        }
+        else
+        {
+            score.text = "Highest reached - " + 0;
+        }
+
     }
 }
